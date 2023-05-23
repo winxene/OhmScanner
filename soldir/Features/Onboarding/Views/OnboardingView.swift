0de6b866a909379df06f282092189479351b2240
winxene <winxeneric@gmail.com>
2023-05-23 20:56:43 +0700

    refactor: make the code cleaner

--- soldir/Features/Onboarding/Views/OnboardingView.swift
+++ soldir/Features/Onboarding/Views/OnboardingView.swift
@@ -9,29 +9,26 @@ import SwiftUI
 
 struct OnboardingView: View {
     var body: some View {
-        GeometryReader {
-            geometry in
-            VStack(alignment: .center){
-                TabView {
-                    OnboardingCard(
-                        imageName: "phone_icon",
-                        titleText: "Know resistor value easily",
-                        descriptionText: "No need to decipher color codes anymore. Just point your camera at the resistor and let us do the work. Say goodbye to manual calculations and hello to hassle-free identification.")
-                    OnboardingCard(
-                        imageName: "watch_icon",
-                        titleText: "The convenient is within your Hand",
-                        descriptionText: "No need to reach for your phone or memorize color codes. With our app for Apple Watch, simply scan any resistor and have its value displayed right on your wrist. Stay hands-free and effortlessly access resistor information wherever you are.")
-                }.tabViewStyle(PageTabViewStyle()).padding(.top, geometry.size.height * 0.174)
-                    .onAppear{
-                        setupAppearance()
-                    }
-                //rounded rectangle button here
-                
+        NavigationView {
+            GeometryReader {
+                geometry in
+                VStack(alignment: .center){
+                    TabView {
+                        OnboardingCard(
+                            imageName: "phone_icon",
+                            titleText: "Know resistor value easily",
+                            descriptionText: "No need to decipher color codes anymore. Just point your camera at the resistor and let us do the work. Say goodbye to manual calculations and hello to hassle-free identification.")
+                        OnboardingCard(
+                            imageName: "watch_icon",
+                            titleText: "The convenient is within your Hand",
+                            descriptionText: "No need to reach for your phone or memorize color codes. With our app for Apple Watch, simply scan any resistor and have its value displayed right on your wrist. Stay hands-free and effortlessly access resistor information wherever you are.")
+                    }.tabViewStyle(PageTabViewStyle()).padding(.top, geometry.size.height * 0.174)
+                        .onAppear{
+                            AppearanceModel.setupAppearance()
+                        }
+                    NavigationButton(destination: HomeView(), label: "Next", geometry: geometry)
+                }
             }
         }
-    }
-    func setupAppearance() {
-      UIPageControl.appearance().currentPageIndicatorTintColor = .systemTeal
-      UIPageControl.appearance().pageIndicatorTintColor = UIColor.black.withAlphaComponent(0.2)
     }
 }
