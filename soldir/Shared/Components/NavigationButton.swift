3cea1497ed4532cb3921ee8c6114be754de7b743
winxene <winxeneric@gmail.com>
2023-05-24 13:36:05 +0700

    fix: fix button won't navigate to the next page

--- soldir/Shared/Components/NavigationButton.swift
+++ soldir/Shared/Components/NavigationButton.swift
@@ -14,14 +14,12 @@ struct NavigationButton<Destination: View>: View {
     
     var body: some View {
         NavigationLink(destination: destination) {
-            Button(action: {}) {
-                Text(label)
-                    .font(.system(size: 24, weight: .bold))
-                    .frame(width: 179, height: 56)
-            }
+            Text(label)
+                .font(.system(size: 24, weight: .bold))
+                .frame(width: 179, height: 56)
+        }
             .buttonStyle(.borderedProminent)
             .accentColor(.teal)
-        }
         .padding(.vertical, geometry.size.width * 0.038)
     }
 }
