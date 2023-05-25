52967d8761157df36e114a8d1e6e45c29d883066
winxene <winxeneric@gmail.com>
2023-05-25 18:26:51 +0700

    refactor: create separate session delegate file to make it cleaner

--- soldir/Shared/Helpers/SessionDelegate.swift
+++ soldir/Shared/Helpers/SessionDelegate.swift
@@ -5,4 +5,17 @@
 //  Created by Winxen Ryandiharvin on 25/05/23.
 //
 
-import Foundation
+import WatchConnectivity
+
+class SessionDelegate: NSObject, WCSessionDelegate {
+    func sessionDidBecomeInactive(_ session: WCSession) {
+        session.activate()
+    }
+    
+    func sessionDidDeactivate(_ session: WCSession) {
+        session.activate()
+    }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+    }
+}
