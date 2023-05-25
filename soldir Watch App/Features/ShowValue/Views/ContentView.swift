42c0d1abd451ec9a663559c8a5206bf63332670d
winxene <winxeneric@gmail.com>
2023-05-25 17:24:34 +0700

    feat: added watchconnectivity

--- soldir Watch App/Features/ShowValue/Views/ContentView.swift
+++ soldir Watch App/Features/ShowValue/Views/ContentView.swift
@@ -6,17 +6,51 @@
 //
 
 import SwiftUI
+import WatchConnectivity
+
+class SessionDelegate: NSObject, WCSessionDelegate {
+    weak var viewModel: ContentViewModel?
+    
+    init(viewModel: ContentViewModel) {
+        self.viewModel = viewModel
+    }
+    
+    func session(_ session: WCSession, activationDidCompleteWith activationState: WCSessionActivationState, error: Error?) {
+    }
+    
+    func session(_ session: WCSession, didReceiveMessage message: [String: Any]) {
+        if let receivedResistorValue = message["resistorValue"] as? String {
+            DispatchQueue.main.async { [weak self] in
+                self?.viewModel?.resistorValue = receivedResistorValue
+            }
+        }
+    }
+}
+
+class ContentViewModel: ObservableObject {
+    @Published var resistorValue: String = ""
+}
 
 struct ContentView: View {
+    @ObservedObject var viewModel = ContentViewModel()
+    @State private var sessionDelegate: SessionDelegate?
+
     var body: some View {
         VStack {
             Text("Resistance").fontWeight(.bold).padding(.bottom, 16)
             HStack(alignment: .center, spacing: 12){
-                //pakai viewmodels bwt atur fontnya
-                Text("100")
+                Text(viewModel.resistorValue)
                 Text("Ω")
             }
         }
         .padding()
+        .onAppear {
+            if WCSession.isSupported() {
+                let session = WCSession.default
+                sessionDelegate = SessionDelegate(viewModel: viewModel)
+                session.delegate = sessionDelegate
+                session.activate()
+            }
+        }
     }
 }
